68ce725525d5ba54d6beabdc4b1e74256324b2d3
Tormy Van Cool <32305535+tormyvancool@users.noreply.github.com>
2025-11-17 16:27:57 +0100

    .

--- custom-global-variables.php
+++ custom-global-variables.php
@@ -5,7 +5,7 @@
  * Description:   Easily create custom variables that can be accessed globally in WordPress and PHP with optional comments per variable.
  * Version:       2.0.2
  * Stable tag:    2.0.2
- * Author:        Tormy Van Cool
+ * Author:        Tormy Van Cool (I took over from the original author: akirak)
  * Author URI:    https://www.newtarget.com
  * License:       GPL2 or later
  * License URI:   https://www.gnu.org/licenses/gpl-2.0.html
